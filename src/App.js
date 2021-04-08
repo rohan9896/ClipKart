069660a40a345df38690ed3db9c84661c1f33f40
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-04-09 01:09:26 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import './App.css';
-import NavBar from "./Components/NavBar/NavBar"
-import LandingPage from './Components/LandingPage/LandingPage';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import BottomNav from "./Components/BottomNav/BottomNav";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <LandingPage />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <NavBar />
+              <LandingPage />
+            </>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <>
+              <NavBar />
+              <h1>Products will come here..</h1>
+              <BottomNav />
+            </>
+          }
+        />
+      </Routes>
     </div>
   );
 }
 
 export default App;
 
+// {/* <Search /> */}
+// {/* <Heading /> */}
+// {/* <CartPage /> */}
+// {/* <WishListPage /> */}
+// {/* <ProductCard name="rohan" description="A very good prod" price="150 rupeya" inStock={false} isNew={false} img="https://bit.ly/2NY8Tx4" rating="0.5" /> */}
 
-      // {/* <Search /> */}
-      // {/* <Heading /> */}
-      // {/* <CartPage /> */}
-      // {/* <WishListPage /> */}
-      // {/* <ProductCard name="rohan" description="A very good prod" price="150 rupeya" inStock={false} isNew={false} img="https://bit.ly/2NY8Tx4" rating="0.5" /> */}
-
-      // {/* <EmptyPage img="https://raw.githubusercontent.com/rohan9896/Testing-for-CSS-component-library/23abfd6d1dc3f4df585291366274fc516071ee22/icons/ecomm/emptyCart.svg" pageType="Cart" /> */}
-      // {/* <EmptyPage img="https://raw.githubusercontent.com/rohan9896/Testing-for-CSS-component-library/551189fca98a388829785311f690d06c18ed8be9/icons/ecomm/empty_wishlist.svg" pageType="Wishlist" /> */}
-      // {/* <BottomNav /> */}
+// {/* <EmptyPage img="https://raw.githubusercontent.com/rohan9896/Testing-for-CSS-component-library/23abfd6d1dc3f4df585291366274fc516071ee22/icons/ecomm/emptyCart.svg" pageType="Cart" /> */}
+// {/* <EmptyPage img="https://raw.githubusercontent.com/rohan9896/Testing-for-CSS-component-library/551189fca98a388829785311f690d06c18ed8be9/icons/ecomm/empty_wishlist.svg" pageType="Wishlist" /> */}
+// {/* <BottomNav /> */}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import CartWishListContextProvider from "./context/cart-wishlist-context";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <CartWishListContextProvider>
-      <App />
-    </CartWishListContextProvider>
+    <Router>
+      <CartWishListContextProvider>
+        <App />
+      </CartWishListContextProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
