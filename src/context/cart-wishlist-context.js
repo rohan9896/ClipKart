75f80798638518a8d28c76960623f3a2b37d7348
Rohan Gupta <56235204+rohan9896@users.noreply.github.com>
2021-05-05 18:27:39 +0530

    fix: chaged name of response data variable

--- src/context/cart-wishlist-context.js
+++ src/context/cart-wishlist-context.js
@@ -17,7 +17,7 @@ export default function CartWishListContextProvider({ children }) {
       );
 
       if (resp.data.success) {
-        const productsData = resp.data.productsData;
+        const productsData = resp.data.products;
         dispatch({ type: "SET_PRODUCTS_DATA", payload: productsData }); //for setting the state of products
         setApiData(productsData);  //for storing the original products data recived from serevr
         setIsProductsReceived(true);
